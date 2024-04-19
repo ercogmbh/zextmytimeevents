00231e94d06ba7432fd7be9038ff4c7c3ea7f6ce
Jaime Ojeda <jaime.ojeda@nubexx.com>
2024-04-19 13:28:40 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,21 @@
+jQuery.sap.declare("hcm.fab.mytimeevents.com.erco.timeevents.Component");
+
+
+(function() {
+	jQuery.sap.registerModulePath("sap.hcm.lib.common", "/sap/bc/ui5_ui5/sap/hcm_common/sap/hcm/lib/common/");
+	}());
+
+// use the load function for getting the optimized preload file if present
+sap.ui.component.load({
+	name: "hcm.fab.mytimeevents",
+	// Use the below URL to run the extended application when SAP-delivered application is deployed on SAPUI5 ABAP Repository
+	url: "/sap/bc/ui5_ui5/sap/HCMFAB_TIME_EVT"
+		// we use a URL relative to our own component
+		// extension application is deployed with customer namespace
+});
+
+this.hcm.fab.mytimeevents.Component.extend("hcm.fab.mytimeevents.com.erco.timeevents.Component", {
+	metadata: {
+		manifest: "json"
+	}
+});
